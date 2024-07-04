c32169e024e80841433f57bc3e47da22312ae33e
Md Azad Hossain <sokalazad@gmail.com>
2024-07-04 17:01:34 +0200

    added sign up functionality for authority

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUP/SignUp";
+import AuthSign from "../pages/AuthSign/AuthSign";
 
 export const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
         {
             path: "/signup",
             element: <SignUp></SignUp>
+        },
+        {
+            path: "/authsignup",
+            element: <AuthSign></AuthSign>
         }
       ]
     },
